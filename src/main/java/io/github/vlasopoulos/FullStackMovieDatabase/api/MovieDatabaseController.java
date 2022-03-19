46a36bb210557b0397344cb1b6e1f16e8410c17a
Vasilis V <f3v3r.ath.cx@gmail.com>
2022-03-19 10:13:18 +0200

    changed endpoint to /api/v1

--- src/main/java/io/github/vlasopoulos/FullStackMovieDatabase/api/MovieDatabaseController.java
+++ src/main/java/io/github/vlasopoulos/FullStackMovieDatabase/api/MovieDatabaseController.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping(path = "api/")
+@RequestMapping(path = "api/v1/")
 public class MovieDatabaseController {
 
     private final MovieDatabaseService movieDatabaseService;
