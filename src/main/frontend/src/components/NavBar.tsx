5fde7714add5fbfe1eded8f4ca17bde62f4ab014
Vasilis V <f3v3r.ath.cx@gmail.com>
2022-03-21 09:44:33 +0200

    added logo and favicon

--- src/main/frontend/src/components/NavBar.tsx
+++ src/main/frontend/src/components/NavBar.tsx
@@ -1,20 +1,22 @@
 import React from 'react'
 import { Link } from "react-router-dom";
-
+import Search from "./Search"
 type Props = {}
 
 const NavBar = (props: Props) => {
   return (
-      <div className='navbar'>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="watchlist">Watchlist</Link></li>
-            <li><Link to="watched">Watched</Link></li>
-            <li><Link to="about">About</Link></li>
-          </ul>
-        </nav>
-      </div>
+    <div className='navbar'>
+      <img src="logo.png" alt="Logo" />
+      <Search />
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="watchlist">Watchlist</Link></li>
+          <li><Link to="watched">Watched</Link></li>
+          <li><Link to="about">About</Link></li>
+        </ul>
+      </nav>
+    </div>
   )
 }
 
