68ac6e8e6d80a77c4443c81932686a06198a7b02
Vasilis V <f3v3r.ath.cx@gmail.com>
2022-03-23 11:33:10 +0200

    added user tables, updated sql queries to include user tables

--- src/main/java/io/github/vlasopoulos/FullStackMovieDatabase/api/rowMappers/TitleRowMapper.java
+++ src/main/java/io/github/vlasopoulos/FullStackMovieDatabase/api/rowMappers/TitleRowMapper.java
@@ -31,31 +31,6 @@ public class TitleRowMapper implements RowMapper<Title> {
         );
     }
 
-    public Title mapRowV2(ResultSet rs, int rowNum) throws SQLException {
-        List<String> genres,directors,writers;
-        if (rs.getArray("genres").toString().equals("{NULL}")) genres = List.of();
-        else genres = List.of((String[])rs.getArray("genres").getArray());
-        if (rs.getArray("directors").toString().equals("{NULL}")) directors = List.of();
-        else directors = List.of((String[])rs.getArray("directors").getArray());
-        if (rs.getArray("writers").toString().equals("{NULL}")) writers = List.of();
-        else writers = List.of((String[])rs.getArray("writers").getArray());
-        return new Title(
-                rs.getString("tconst"),
-                rs.getString("title_type"),
-                rs.getString("primary_title"),
-                rs.getString("original_title"),
-                intToBoolean(rs.getInt("is_adult")),
-                rs.getInt("start_year"),
-                rs.getInt("end_year"),
-                rs.getInt("runtime_minutes"),
-                genres,
-                directors,
-                writers,
-                rs.getFloat("average_rating"),
-                rs.getInt("num_votes")
-        );
-    }
-
     private boolean intToBoolean(int n) {
         return (n==1);
     }
