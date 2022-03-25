54cab82a2383cdef1ece86103dc5455842c88296
Vasilis V <f3v3r.ath.cx@gmail.com>
2022-03-25 10:06:45 +0200

    updated home

--- src/main/frontend/src/pages/Home.tsx
+++ src/main/frontend/src/pages/Home.tsx
@@ -15,8 +15,21 @@ const Home = (props: Props) => {
           <img src="docker-logo.png" alt="docker logo" />
           <img src="react-logo.png" alt="react logo" />
         </div>
-        <p>Frontend listens at /<br />Backend listens at /api/v1/</p>
-        <p></p>
+        <div className='text-container'>
+          <p>The backend is written in Java using the Spring Boot framework and connects to the Postgres database
+            using <code>JDBCTemplate</code>. It's containerized in docker with docker-compose using the postgres 
+            image for the database and a custom image that runs the app.
+          </p>
+          <p>The app is packed as a whole in one .war package, containing the backend and the frontend.<br />
+          Frontend listens at /<br />Backend listens at /api/v1/</p>
+          <p>The frontend is written in React using Typescript.</p>
+          <p>The data comes from the publicly available <a href='https://www.imdb.com/interfaces/'>IMDB datasets</a> that, according
+          to IMDB, are daily updated. The app downloads the .gz files, extracts them, processes 
+          them to remove errors and upserts them into the database.</p>
+          <p>The user can search titles and persons, add titles to watched/watchlist and rate them.</p>
+          <p>Searching is implemented using the <code>tsvector</code> and <code>tsquery</code> capabilities
+          of Postgres.</p>
+        </div>
       </div>
     </div>
   )
