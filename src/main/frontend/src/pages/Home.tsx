af8ca99d5f99cd27002b6a181c982293ec2fd78b
Vasilis V <f3v3r.ath.cx@gmail.com>
2022-03-25 09:43:59 +0200

    started writing home page

--- src/main/frontend/src/pages/Home.tsx
+++ src/main/frontend/src/pages/Home.tsx
@@ -5,7 +5,19 @@ type Props = {}
 const Home = (props: Props) => {
   return (
     <div className='margin-page'>
-    <div className='page'>Home</div>
+      <div className='page'>
+        <h1 className='h1-centered'>Full stack movie database</h1>
+        <p className='p-centered'>Technologies used:</p>
+        <div className='image-container'>
+          <img src="java-logo.png" alt="java logo" />
+          <img src="spring-boot-logo.png" alt="spring boot logo" />
+          <img src="postgres-logo.png" alt="postgres logo" />
+          <img src="docker-logo.png" alt="docker logo" />
+          <img src="react-logo.png" alt="react logo" />
+        </div>
+        <p>Frontend listens at /<br />Backend listens at /api/v1/</p>
+        <p></p>
+      </div>
     </div>
   )
 }
